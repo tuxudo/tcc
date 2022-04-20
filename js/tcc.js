ebb2fe2d96a9ebb331c08248a41f8bc69c383ebc
tuxudo <tuxudo@users.noreply.github.com>
2022-04-20 15:06:54 -0400

    Updated for Monterey
    Fixed lots of widget bugs too

--- js/tcc.js
+++ js/tcc.js
@@ -2,7 +2,7 @@ var formatTCC_service = function(col, row){
     
     var cell = $('td:eq('+col+')', row),
         value = cell.text();
-    $(cell).html(i18n.t('tcc.'+value.replace("kTCCServiceSystemPolicy","").replace("kTCCService","")).replace("tcc.",""))
+    $(cell).html('<span title="'+value+'">'+i18n.t('tcc.'+value.replace("kTCCServiceSystemPolicy","").replace("kTCCService","")).replace("tcc.","")+'</span>')    
 };
 
 var formatTCCYesNo = function(col, row){
